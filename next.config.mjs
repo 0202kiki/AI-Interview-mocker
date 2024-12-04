322e867f945100a7ddebc063569e66a8c2658aaa
0202kiki <kirthikasundari4@gmail.com>
2024-12-04 13:14:17 +0530

    Add redirect from / to /dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/dashboard', // Adjust this path to your dashboard route
+          permanent: true, // Set to true if this redirect is permanent
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
